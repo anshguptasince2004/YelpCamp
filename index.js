46e8439fc33f31e964f06d9d0ffe9d3ad0234b79
Ansh Gupta <anshgupta3221@gmail.com>
2025-08-03 17:21:19 +0530

    Configured sessions and added partials

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const session = require('express-session');
 const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError');
+const flash = require('connect-flash');
 
 const campgrounds = require('./routes/campgrounds.js')
 const reviews = require('./routes/reviews.js')
@@ -21,10 +23,31 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 app.engine('ejs', ejsMate)
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+const sessionConfig = {
+    secret: 'thisisasecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000*60*60*24*7,
+        maxAge: 1000*60*60*24*7,
+    }
+}
+
+app.use(session(sessionConfig));
+
+app.use(flash());
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
 
 app.use("/campgrounds", campgrounds)
 app.use("/campgrounds/:id/reviews", reviews)
-app.use(express.static(path.join(__dirname, 'public')))
 
 app.all(/(.*)/, (req, res, next) => {
     next(new ExpressError('Page Not Found', 404))

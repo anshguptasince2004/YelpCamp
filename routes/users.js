03601db98d9196dd3894d87cd6a217aa82158344
Ansh Gupta <anshgupta3221@gmail.com>
2025-08-15 21:51:15 +0530

    Updated routes structure

--- routes/users.js
+++ routes/users.js
@@ -6,13 +6,13 @@ const passport = require('passport');
 const { storeReturnTo } = require('./middleware');
 const users = require('../controllers/users');
 
-router.get('/register', users.renderRegisterForm);
+router.route('/register')
+.get(users.renderRegisterForm)
+.post(catchAsync( users.registerUser));
 
-router.post('/register', catchAsync( users.registerUser));
-
-router.get('/login', users.renderLoginForm);
-
-router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.loginUser);
+router.route('/login')
+.get(users.renderLoginForm)
+.post(storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.loginUser);
 
 router.get('/logout', users.logoutUser);
 

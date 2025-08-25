37c91d0bbc39b8de75151c75ad47c337a688eb93
Ansh Gupta <anshgupta3221@gmail.com>
2025-08-25 23:57:18 +0530

    added mapbox pseudocodes - 1

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,5 +1,8 @@
 const Campground = require('../models/campground');
 const { cloudinary } = require('../cloudinary');
+// const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+// const mapBoxToken = process.env.MAPBOX_TOKEN;
+// const geocoder = mvbxGeocoding({ accessToken: mapBoxToken });
 
 module.exports.index = async (req, res) => {
     const campgrounds = await Campground.find({})
@@ -12,8 +15,14 @@ module.exports.newCampgroundForm = (req, res) => {
 }
 
 module.exports.createCampground = async (req, res, next) => {
+    // const geoData = await geocoder.forwardGeocode({
+    //     query: req.body.campground.location,
+    //     limit: 1
+    // }).send()
+
     console.log('create campground')
     const campground = new Campground(req.body.campground);
+    // campground.geometry = geoData.body.features[0].geometry;
     campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.author = req.user._id;
     await campground.save();

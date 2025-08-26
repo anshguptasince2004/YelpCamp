6ad7d058f898fb2c4205f8a2a33ef7fd0bba206e
Ansh Gupta <anshgupta3221@gmail.com>
2025-08-26 22:08:38 +0530

    added mapTiler - working code

--- models/campground.js
+++ models/campground.js
@@ -12,22 +12,22 @@ ImageSchema.virtual('thumbnail').get(function () {
     return this.url.replace('/upload', '/upload/w_200');
 })
 
-// const opts = { toJSON: { virtuals: true } };
+const opts = { toJSON: { virtuals: true } };
 
 const CampgroundSchema = new Schema({
     title: String,
     images: [ImageSchema],
-    // geometry: {
-    //     type: {
-    //         type: String,
-    //         enum: ['Point'],
-    //         required: true
-    //     },
-    //     coordinates: {
-    //         type: [Number],
-    //         required: true
-    //     }
-    // },
+    geometry: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    },
     price: Number,
     description: String,
         location: String,
@@ -41,15 +41,12 @@ reviews: [
         ref: 'Review'
     }
 ],
-}
-// , opts
-);
+}, opts);
 
-// CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
-//     return `<strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>
-//     <p>${this.description.substring(0, 25)}...</p>`;
-
-
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    return `<strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>
+    <p>${this.description.substring(0, 25)}...</p>`;
+})
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
     if (doc) {
--- seeds/index.js
+++ seeds/index.js
@@ -25,13 +25,13 @@ const seedDB = async () => {
             title: `${(sample(descriptors))} ${(sample(places))}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae provident ipsam, possimus ex dolor sequi quia odio veniam nihil rerum ratione nisi labore? Praesentium a tenetur possimus quis error saepe.',
             price,
-            // geometry: {
-            //     type: "Point",
-            //     coordinates: [
-            //         cities[random1000].longitude,
-            //         cities[random1000].latitude,
-            //     ]
-            // },
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude,
+                ]
+            },
             images: [
                 {
                     url: 'https://res.cloudinary.com/dfdolahjd/image/upload/v1756011409/YelpCamp/dbb8gtzpsfnz7nru4tjq.png',
